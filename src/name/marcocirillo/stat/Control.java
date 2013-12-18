91dab7353d1452503b108d2ce2e13f48db4bfde8
MarkyC <mmmarcooo@gmail.com>
2013-12-18 17:20:44 -0500

    Create From Input File Mode Completed

---
+++ src/name/marcocirillo/stat/Control.java
@@ -0,0 +1,118 @@
+package name.marcocirillo.stat;
+
+import java.io.File;
+import java.util.EnumMap;
+import java.util.List;
+
+/**
+ * User: Marco
+ * Email: cirillom@yorku.ca
+ * Date: 12/17/13 5:04 PM.
+ * <p/>
+ * Description
+ * <p/>
+ * <p/>
+ * <p/>
+ * Created with IntelliJ IDEA.
+ */
+public class Control {
+
+    private static File inputFile;
+    private static File outputFile;
+    private static List<Stat> stats;
+
+    private static EnumMap<Arguments, String> arguments;
+
+    /**
+     * All the arguments available to the program.
+     * These can be passed as a key to getArgument(
+     */
+    public static enum Arguments {
+        INPUT, OUTPUT, LIVE
+    }
+
+    private static boolean addArg(Arguments key, String val) {
+        arguments.put(key, val);
+        System.out.println(val);
+        return !((val == null) || (val.length() == 0));
+    }
+
+    private static boolean parseArgs(String[] args) {
+        arguments = new EnumMap<Arguments, String>(Arguments.class);
+
+        for (int i = 0; i < args.length-1; i+=2) {
+            switch (args[i].charAt(0)) {
+                // We parse arguments by checking for -'s representing the start of an arg
+                case '-':
+                    if (args[i].length() < 2) return false;
+
+                    // Purposely not using Java 7's String switches for backwards compatibility
+                    String s = args[i].substring(1);
+                    if (s.equals("i")) {
+                        if (!addArg(Arguments.INPUT, args[i+1])) return false;
+                    } else if (s.equals("o")) {
+                        if (!addArg(Arguments.OUTPUT, args[i+1])) return false;
+                    } else if (s.equals("live")) {
+                        if (!addArg(Arguments.LIVE, args[i+1])) return false;
+                    } else {
+                        return false;
+                    }
+                break;
+
+                default:
+                   return false;
+            }
+        }
+
+
+        return arguments.containsKey(Arguments.INPUT) || arguments.containsKey(Arguments.LIVE);
+    }
+
+    public static void printUsage() {
+        System.out.println("Usage: java -jar stat.jar [-i <InputFile.blg>] [-live] [-o <OutputFile.csv>]");
+        System.out.println("       Either -live must be chosen (for live monitoring of the system) ");
+        System.out.println("       or -i <InputFile.blg> (for creating output from an input file)  ");
+        System.out.println("       must be used. If an output file is not specified, the results   ");
+        System.out.println("       outputted to stdout                                             ");
+    }
+
+    public static void setInputFile(String filename) {
+        inputFile = (filename != null) ? new File(filename) : null;
+    }
+
+    public static void setOutputFile(String filename) {
+        outputFile = (filename != null) ? new File(filename) : null;
+    }
+
+    public static void main(String[] args) {
+
+        if (!parseArgs(args)) exit(true);
+
+        setInputFile(arguments.get(Arguments.INPUT));
+        setOutputFile(arguments.get(Arguments.OUTPUT));
+
+        if (arguments.containsKey(Arguments.INPUT)) {
+            // Input File Mode
+            try {
+                stats = Stat.createFromInput(inputFile);
+            } catch (UnsupportedOperationException unsupported) {
+                unsupported.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            // Live Mode
+            throw new UnsupportedOperationException("Live mode is not implemented yet.");
+        }
+
+        for (Stat s : stats) {
+            System.out.println(s);
+        }
+
+    }
+
+    public static void exit(boolean printUsage) {
+        if (printUsage) printUsage();
+        System.exit(1);
+    }
+}
--- src/name/marcocirillo/stat/Stat.java
+++ src/name/marcocirillo/stat/Stat.java
@@ -1,9 +1,19 @@
 package name.marcocirillo.stat;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
- * User: Marco
+ * User: Marco Cirillo
  * Email: cirillom@yorku.ca
- * Date: 12/17/13 4:06 PM.
  * <p/>
  * Description
  * <p/>
@@ -12,4 +22,151 @@ package name.marcocirillo.stat;
  * Created with IntelliJ IDEA.
  */
 public class Stat {
+
+    private final String name;
+    private final String process;
+    private final Map<Date, Double> values;
+
+    public Stat(String process, String name) {
+        this.process = process;
+        this.name = name;
+        this.values = new HashMap<Date, Double>();
+    }
+
+    public void addValue(Date time, Double value) {
+        this.values.put(time, value);
+    }
+
+    public Map<Date, Double> getValues() {
+        return this.values;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getProcess() {
+        return this.process;
+    }
+
+    @Override
+    public String toString() {
+        String result = "Stat [process=" + getProcess() + " name=" + getName() + " values=[";
+
+        // Show values
+        for (Map.Entry<Date, Double> item : getValues().entrySet()) {
+            result += item.getKey() + "=" + item.getValue() + ", ";
+        }
+
+        // Remove trailing comma and space and add square closing brackets
+        result = result.substring(0, result.length() - 2) + "]]";
+
+        return result;
+    }
+
+    public static List<Stat> createFromInput(File input)
+            throws IOException, IllegalArgumentException, UnsupportedOperationException {
+
+        // Verify the file is okay to use
+        if (!input.exists() || input.length() == 0) {
+            throw new IllegalArgumentException("Input File is invalid.");
+        }
+
+        // Handle input file types primitively (by extension)
+        if (getExtension(input).equalsIgnoreCase("blg")) {
+            return createFromBLG(input);
+        } else {
+            // TODO: Handle more file types
+            throw new UnsupportedOperationException("Currently, only Windows BLG files are supported.");
+        }
+    }
+
+    private static String getExtension(File input) {
+        return input.getName().substring(input.getName().indexOf(".") + 1);
+    }
+
+    private static List<Stat> createFromBLG(File input) throws IOException {
+        // Currently windows only, as relog is required
+        if (!System.getProperty("os.name").contains("Windows"))
+            throw new UnsupportedOperationException("Linux input is not implemented yet.");
+
+        List<Stat> result = new ArrayList<Stat>();
+
+        // Parse CSV for Stats
+
+        // Create input stream, throws FileNotFoundException if input cannot be found
+        BufferedReader in = new BufferedReader(new FileReader(BLGtoCSV(input)));
+
+        // Read and create Stat objects
+        String line = in.readLine();
+        if (line == null) throw new IllegalArgumentException("Could not parse CSV file for Stats");
+
+        String[] names = line.split(",");       // split along commans
+        for (int i = 1; i < names.length; i++) {// skip first column (header for timestamps)
+
+            // Match \\COMPUTER\Process(processname)\Attribute via the parenthesis
+            Matcher match = Pattern.compile("\\([^)]+\\)").matcher(names[i]);
+            if (match.find()) {
+                result.add( new Stat(
+                        match.group().replace("(", "").replace(")",""), // remove parenthesis when adding
+                        names[i].substring(names[i].lastIndexOf("\\") + 1)
+                ));
+            } else {
+                throw new IllegalArgumentException("Could not parse CSV file for Stats");
+            }
+        }
+
+        // Read values into Stats
+        while ( null != ( line = in.readLine() ) ) {
+
+            String[] values   = line.split(",");
+
+            // example: 12/05/2013 19:40:44.951
+            DateFormat format = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss.SSS");
+            Date time = null;
+            try {
+                time = format.parse( values[0].replaceAll("\"","") );
+            } catch (ParseException e) {
+                System.out.println("Could not parse Date from CSV");
+                e.printStackTrace();
+            }
+
+            for (int i = 1; i < result.size(); i++) {
+                try {
+                    // Add to Stat, removing quotes around each entry
+                    result.get(i).addValue( time, Double.parseDouble( values[i].replaceAll("\"", "") ));
+                } catch (NumberFormatException e) {
+                    // Account for "", which means no stat was reported for that time
+                    result.get(i).addValue( time, 0d );
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private static File BLGtoCSV(File blg) throws IOException {
+        // Build CSV from BLG
+
+        String filename = blg.getName().substring(0, blg.getName().length() - 4);
+        File csv = File.createTempFile(filename,".csv");
+
+        String[] cmd = {
+                "relog",
+                "-i", blg.getAbsolutePath(),
+                "-f", "csv",
+                "-o", csv.getAbsolutePath(),
+                "-y"
+        };
+
+        try {
+            Process p = Runtime.getRuntime().exec(cmd);
+            p.waitFor();
+        } catch (InterruptedException e) {
+            System.out.println("Conversion from BLG to CSV was prematurely interrupted.");
+            e.printStackTrace();
+        }
+
+        return csv;
+    }
 }
